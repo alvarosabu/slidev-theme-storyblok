4d76a9af199670706e3203dd2e4346d75774f38d
alvarosabu <alvaro.saburido@gmail.com>
2023-08-24 19:57:29 +0200

    feat: add storyblok branded default layout

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="slidev-layout default" :class="$attrs.class">
+    <SbLogo class="w-200px -ml-8 mb-16"/>
+    <div class="w-full prose">
+        <slot />
+    </div>
+  </div>
+</template>
