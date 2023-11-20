0e44dd842c06b8d040446c41ac899efc2ef59cb2
alvarosabu <alvaro.saburido@gmail.com>
2023-11-20 12:39:03 +0100

    fix: end layout css

--- layouts/end.vue
+++ layouts/end.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="slidev-layout end">
-      <div class="my-auto">
-        <slot />
-        <logos-storyblok-icon class="filter-grayscale opacity-05 absolute right-20% bottom-50% transform scale-3000 -rotate-20"/>
-      </div>
+  <div class="slidev-layout end">
+    <div class="my-auto">
+      <slot />
+      <logos-storyblok-icon class="filter-grayscale opacity-05 absolute right-20% bottom-50% transform scale-3000 -rotate-20"/>
     </div>
-  </template>
+  </div>
+</template>
   
